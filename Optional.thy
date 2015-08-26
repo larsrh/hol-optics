f21f44fe45560d167af8a300d1fc8676fd20cea3
Lars Hupel <lars.hupel@mytum.de>
2015-08-26 22:21:03 +0200

    fix the Prism -> Optional construction

--- Optional.thy
+++ Optional.thy
@@ -4,7 +4,7 @@ begin
 
 locale optional =
   fixes get' :: "'s \<Rightarrow> 'a option" and set :: "'a \<Rightarrow> 's \<Rightarrow> 's"
-  assumes set_get'[simp]: "get' (set a s) = Some a"
+  assumes set_get'[simp]: "get' (set a s) = map_option (\<lambda>_. a) (get' s)"
   assumes get'_set[simp]: "get' s = Some a \<Longrightarrow> set a s = s"
   assumes set_set[simp]: "set a (set a' s) = set a s"
 begin
@@ -40,18 +40,23 @@ end
 context prism begin
 
 definition set :: "'a \<Rightarrow> 's \<Rightarrow> 's" where
-"set a _ = back a"
+"set a s = (case get' s of None \<Rightarrow> s | Some _ \<Rightarrow> back a)"
 
 sublocale optional!: optional get' set
-by unfold_locales (auto simp: set_def)
+apply unfold_locales
+apply (auto simp: set_def split: option.splits)
+done
 
 lemma modify_eq[simp]: "optional.modify = modify"
 unfolding modify_def[abs_def] optional.modify_def[abs_def] set_def
-by simp
+by (rule ext)+ (auto split: option.splits)
 
 lemma modify'_eq[simp]: "optional.modify' = modify'"
 unfolding modify'_def[abs_def] optional.modify'_def[abs_def] set_def comp_def
-by auto
+apply (rule ext)+
+apply (case_tac "get' s")
+apply auto
+done
 
 end
 
@@ -61,7 +66,8 @@ lemma get'_eq[simp]: "lens.get' = get'"
 unfolding get'_def lens.get'_def by simp
 
 lemma set_eq[simp]: "prism.set = set"
-unfolding set_def[abs_def] prism.set_def[abs_def] by simp
+unfolding set_def[abs_def] prism.set_def[abs_def]
+by (simp add: get'_def)
 
 end
 
