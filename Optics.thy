c7531be0a76541e6b133c4f974ead006c84b6915
Lars Hupel <lars.hupel@mytum.de>
2015-08-26 22:21:22 +0200

    some example optics instances

--- Optics.thy
+++ Optics.thy
@@ -2,4 +2,36 @@ theory Optics
 imports Optional
 begin
 
+locale id_iso begin
+  definition "get = id"
+  definition "back = id"
+
+  sublocale iso get "back"
+  by unfold_locales (simp add: get_def back_def)+
+end
+
+locale list_nil_prism begin
+  definition "get' = case_list (Some ()) (\<lambda>_ _. None)"
+  definition "back = (\<lambda>_. [])"
+
+  sublocale prism get' "back"
+  by unfold_locales (auto simp: get'_def back_def split: list.splits)
+end
+
+locale list_cons_prism begin
+  definition "get' = case_list None (\<lambda>x xs. Some (x, xs))"
+  definition "back = (\<lambda>(x, xs). x # xs)"
+
+  sublocale prism get' "back"
+  by unfold_locales (auto simp: get'_def back_def split: list.splits)
+end
+
+locale list_head_optional begin
+  definition "get' = case_list None (\<lambda>x _. Some x)"
+  definition set where "set x xs = case_list xs (\<lambda>_ xs. x # xs) xs"
+
+  sublocale optional get' set
+  by unfold_locales (auto simp: get'_def set_def split: list.splits)
+end
+
 end
